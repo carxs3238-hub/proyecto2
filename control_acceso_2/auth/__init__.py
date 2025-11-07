b444991185b96b8a07accb55b11688b04403850a
carxs3238-hub <carxs3238@gmail.com>
2025-11-07 14:19:28 -0500

    Delete control_acceso_2 directory

--- control_acceso_2/auth/__init__.py
+++
@@ -1,9 +0,0 @@
-# Este archivo hace que la carpeta auth sea un paquete Python
-from .auth import login_required, permiso_requerido, obtener_usuario_actual
-from .permissions import *
-
-__all__ = [
-    'login_required', 
-    'permiso_requerido', 
-    'obtener_usuario_actual'
-]
